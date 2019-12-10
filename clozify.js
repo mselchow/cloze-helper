140319b8ead4939537d7acae8eebf43bd1e88e0c
Michael Selchow <michael.selchow@gmail.com>
2019-12-10 09:56:05 -0500

    Add clear confirmation, finish unclozify

--- clozify.js
+++ clozify.js
@@ -11,6 +11,8 @@ function clozify() {
 	var clozeCode = "{1:MCVS:";
 	var question = "";
 
+	//var correctText = "#That's correct!";
+	//var incorrectText = "#Sorry, that's incorrect.";
 	var correctText = "";
 	var incorrectText = "";
 
@@ -24,36 +26,36 @@ function clozify() {
 	if (document.getElementById("ansA").value != "") {
 		//If A is the correct answer, add correct answer Cloze code, otherwise add incorrect answer code
 		if (document.getElementById("ansAA").checked) {
-			currentStep = "%100%" + htmlEscape(document.getElementById("ansA").value) + "#" + correctText;
+			currentStep = "~%100%" + htmlEscape(document.getElementById("ansA").value) + correctText;
 		} else {
-			currentStep = "~" + htmlEscape(document.getElementById("ansA").value) + "#" + incorrectText;
+			currentStep = "~" + htmlEscape(document.getElementById("ansA").value) + incorrectText;
 		}
 		clozeCode += currentStep;
 	}
 	//Add answer text B if not empty
 	if (document.getElementById("ansB").value != "") {
 		if (document.getElementById("ansBB").checked) {
-			currentStep = "%100%" + htmlEscape(document.getElementById("ansB").value) + "#" + correctText;
+			currentStep = "~%100%" + htmlEscape(document.getElementById("ansB").value) + correctText;
 		} else {
-			currentStep = "~" + htmlEscape(document.getElementById("ansB").value) + "#" + incorrectText;
+			currentStep = "~" + htmlEscape(document.getElementById("ansB").value) + incorrectText;
 		}
 		clozeCode += currentStep;
 	}
 	//Add answer text C if not empty
 	if (document.getElementById("ansC").value!= "") {
 		if (document.getElementById("ansCC").checked) {
-			currentStep = "%100%" + htmlEscape(document.getElementById("ansC").value) + "#" + correctText;
+			currentStep = "~%100%" + htmlEscape(document.getElementById("ansC").value) + correctText;
 		} else {
-			currentStep = "~" + htmlEscape(document.getElementById("ansC").value) + "#" + incorrectText;
+			currentStep = "~" + htmlEscape(document.getElementById("ansC").value) + incorrectText;
 		}
 		clozeCode += currentStep;
 	}
 	//Add answer text D if not empty
 	if (document.getElementById("ansD").value != "") {
 		if (document.getElementById("ansDD").checked) {
-			currentStep = "%100%" + htmlEscape(document.getElementById("ansD").value) + "#" + correctText;
+			currentStep = "~%100%" + htmlEscape(document.getElementById("ansD").value) + correctText;
 		} else {
-			currentStep = "~" + htmlEscape(document.getElementById("ansD").value) + "#" + incorrectText;
+			currentStep = "~" + htmlEscape(document.getElementById("ansD").value) + incorrectText;
 		}
 		clozeCode += currentStep;
 	}
@@ -64,45 +66,64 @@ function clozify() {
 
 
 /*
-	This function does the opposite of the remediate function. Given a set of
-	remediation inputs, it will deconstruct it into its original steps.
+	This function does the opposite of the clozify function. Given a
+	Cloze question, it will deconstruct it into its original question & answers.
 */
 
 function unclozify() {
-	// Gets the remediation from whichever field is not empty
-	var remediation = (document.getElementById("correct").value == "") ? document.getElementById("incorrect").value : document.getElementById("correct").value;
+	// Get Cloze code from output box
+	var clozeCode = document.getElementById("clozecode").value;
 
-	// Loop through for all 5 step textboxes
-	for (var i = 1; i <= 5; i++) {
-		// Generate a dynamic regex with i
-		var pattern = new RegExp("Step " + i + "<\\/a><\\/h3>\\n?<div>\\n?([^]*?)<\\/div>", "gm");
+	// Get question text first
+	var pattern = new RegExp("(.*)<br \/>", "gm");
+	var matches = pattern.exec(clozeCode);
+	if (matches != null) {
+		document.getElementById("question").value = matches[1];
+	}
 
-		// Run the expression for a match
-		var matches = pattern.exec(remediation);
+	// Get remaining answer text
+	pattern = new RegExp("S:~(.*)}");
+	var answers_tmp = pattern.exec(clozeCode);
+	// Index 1 represents the capture group
+	var answers = answers_tmp[1].split("~");
 
-		// If match found, update the right element using a dynamic id
-		var id = "step" + i;
-		if (matches != null) {
-			document.getElementById(id).value = matches[1];
+	// Array for answer boxes
+	var letters = ['A', 'B', 'C', 'D'];
+
+	// Loop through as many times as there are answers
+	for (var i = 0; i <= (answers.length - 1); i++) {
+		// Set document ID dynamically based on letters array
+		var id = "ans" + letters[i];
+
+		// Check if this answer is the correct answer
+		if (answers[i].startsWith("%100%")) {
+			// Set radio button
+			document.getElementById(id + letters[i]).checked = true;
+			// Set answer text and slice out "%100%" text
+			document.getElementById(id).value = answers[i].slice(5, answers[i].length);
+		} else {
+			document.getElementById(id).value = answers[i];
 		}
 	}
 }
 
 
 /*
-	This function is called on button click and clears the output textbox.
+	This function is called on button click and clears the output textbox upon user confirmation.
 */
 
 function clearAll() {
-	//Set the output text to blank
-	document.getElementById("clozecode").value = "";
+	if (confirm("Clear all text?")) {
+		//Set the output text to blank
+		document.getElementById("clozecode").value = "";
 
-	//Set the input text to blank
-	document.getElementById("question").value = "";
-	document.getElementById("ansA").value = "";
-	document.getElementById("ansB").value = "";
-	document.getElementById("ansC").value = "";
-	document.getElementById("ansD").value = "";
+		//Set the input text to blank
+		document.getElementById("question").value = "";
+		document.getElementById("ansA").value = "";
+		document.getElementById("ansB").value = "";
+		document.getElementById("ansC").value = "";
+		document.getElementById("ansD").value = "";
+	}
 }
 
 
